366b052923e4761ca88505a4f0a1edd4212f8195
rwilleyii <rwilley@me.com>
2017-11-12 17:13:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code your solutions in this file
 function printBadges(arrayNames){
 
-for(let i = 0; i < arrayNames.length; i++){
-  i // represent the indexes
+  for(let i = 0; i < arrayNames.length; i++){
   arrayNames[i] //represent the element
   console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`)
   }
