d104fafb25b884a036b7483a0d9224569a8d5eff
rwilleyii <rwilley@me.com>
2017-11-12 17:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function printBadges(arrayNames){
+  arrayNames = ["Avi", "Grace", "Alan"]
+
+for(let i = 0; i < arrayNames.length; i++){
+  i // represent the indexes
+  arrayNames[i] //represent the element
+  console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`)
+  }
+}
